9b73c86242bb51ce207704108e1545c85bfa5064
Jordis Man <jordisman@gmail.com>
2020-03-26 22:44:02 -0700

    Added user address

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const UserEntry = ({user}) => {
+const UserInfo = ({user}) => {
   return (
     <div>
       <div>First Name:{user.firstName} LastName:{user.lastName}</div>
       <div>Email:{user.email}</div>
       <div>Phone No.:{user.phone}</div>
+      <div>Address:{user.address}</div>
     </div>
   )
 }
 
-export default UserEntry;
+export default UserInfo;
