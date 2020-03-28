b5aeb56b5ad3bc9ac3a54423a10f2a9cdc69191c
Jordis Man <jordisman@gmail.com>
2020-03-27 17:29:56 -0700

    Edit functions

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,44 +1,81 @@
 import React from 'react';
+import axios from 'axios';
 
-const UserForm = (props) => {
-  return (
-    <div>
-      <h3>User information</h3>
-      <form onSubmit={props.handleSubmit}>
-        <label>First Name:</label>
-        <input
-          type="text"
-          name="firstName"
-          onChange={props.handleInputChange}
-        />
-        <label>Last Name:</label>
-        <input
-          type="text"
-          name="lastName"
-          onChange={props.handleInputChange}
-        />
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          onChange={props.handleInputChange}
-        />
-        <label>Phone No.</label>
-        <input
-          type="number"
-          name="phone"
-          onChange={props.handleInputChange}
-        />
-        <label>Address</label>
-        <input
-          type="text"
-          name="address"
-          onChange={props.handleInputChange}
-        />
-        <button type="submit" value="submit">REGISTER</button>
-      </form>
-    </div>
-  )
+class UserForm extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      address: ''
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('this.state:', this.state);
+
+    let {firstName, lastName, email, phone, address} = this.state;
+    let input = {firstName, lastName, email, phone, address};
+    let headers = {'Content-Type': 'application/json'}
+    axios.post('/user', input, {headers: headers})
+      .then(res => {
+        console.log(res)
+      })
+
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>User information</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label>First Name:</label>
+          <input
+            type="text"
+            name="firstName"
+            onChange={this.handleInputChange}
+          />
+          <label>Last Name:</label>
+          <input
+            type="text"
+            name="lastName"
+            onChange={this.handleInputChange}
+          />
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            onChange={this.handleInputChange}
+          />
+          <label>Phone No:</label>
+          <input
+            type="number"
+            name="phone"
+            onChange={this.handleInputChange}
+          />
+          <label>Address</label>
+          <input
+            type="text"
+            name="address"
+            onChange={this.handleInputChange}
+          />
+          <button type="submit" value="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default UserForm;
