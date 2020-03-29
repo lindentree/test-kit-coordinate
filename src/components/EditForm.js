c2fed16bdd109e51afe1d0b3f915ef97bdb00298
Jordis Man <jordisman@gmail.com>
2020-03-28 20:49:12 -0700

    Created provider edit profile form

---
+++ src/components/EditForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, Container, H1, Input, Form, Label} from './style.js';
+
+class EditForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+}
+
+export default EditForm;
